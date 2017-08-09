e17bf265b78e75bf679571fa35a1ba94015a2fa1
317uvezpay <317uvezpay@gmail.com>
2017-08-09 04:27:24 +0000

    require

--- 317uvezpay/hello.lua
+++ 317uvezpay/hello.lua
@@ -1,1 +1,27 @@
-return "<h1>Helo, World, STAUFMAN GALAZY!! GREAT BOT</h1>", {["Content-Type"]="text/html"}
+local template =
+[[
+<!doctype html>
+<html>
+<head>
+	<title>{{feed.title}}</title>
+</head>
+<body>
+	<h1><a href="{{{feed.link}}}">{{feed.title}}</a></h1>
+	<ul>
+	{{#entries}}
+		<li><a href="{{{link}}}">{{title}}</a></li>
+	{{/entries}}
+	</ul>
+</body>
+</html>
+]]
+ 
+local feedparser = require 'feedparser'
+local response = http.request { url =
+	request.query.feed or 'http://blog.webscript.io/rss.xml'
+}
+local parsed = feedparser.parse(response.content)
+ 
+local lustache = require 'lustache'
+return lustache:render(template, parsed),
+	{['Content-Type']='text/html; charset=utf-8'}
