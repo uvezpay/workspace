237e6d3159b01eea67415efc435c07717aa58bef
317uvezpay <317uvezpay@gmail.com>
2017-08-09 04:29:16 +0000

    req

--- 317uvezpay/hello.lua
+++ 317uvezpay/hello.lua
@@ -3,25 +3,15 @@ local template =
 <!doctype html>
 <html>
 <head>
-	<title>{{feed.title}}</title>
+	<title>blimey</title>
 </head>
 <body>
-	<h1><a href="{{{feed.link}}}">{{feed.title}}</a></h1>
-	<ul>
-	{{#entries}}
-		<li><a href="{{{link}}}">{{title}}</a></li>
-	{{/entries}}
-	</ul>
+ bloody cricket
 </body>
 </html>
 ]]
  
-local feedparser = require 'feedparser'
-local response = http.request { url =
-	request.query.feed or 'http://blog.webscript.io/rss.xml'
-}
-local parsed = feedparser.parse(response.content)
- 
+
 local lustache = require 'lustache'
-return lustache:render(template, parsed),
+return lustache:render(template),
 	{['Content-Type']='text/html; charset=utf-8'}
