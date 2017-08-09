49f329322fc596c57d6e21b38a9f7fe5d245396e
317uvezpay <317uvezpay@gmail.com>
2017-08-09 16:29:47 +0000

    lustache test

--- 317uvezpay/hello.lua
+++ 317uvezpay/hello.lua
@@ -1,1 +1,35 @@
-return "Back to square 1"
+local view = {
+	title = "Open Source Hello or Goodbye",
+	msg = storage.msg,
+	entries = {{
+		link = 'http://www.bombaytasty.com/wp-content/uploads/2014/10/Chapati.jpg',
+		title = 'Roti'}, {
+		link = 'http://yummyindiankitchen.com/wp-content/uploads/2016/11/masur-ki-dal-massor-daal.jpg',
+		title = 'Daal'}, {
+		link = 'www.yahoo.com',
+		title = 'YAHOO'
+	}}
+}
+
+local template = [[
+<!doctype html>
+<html>
+<head>
+	<title>{{title}}</title>
+
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+	<!-- Mobile Display Optimization-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+{{msg}}
+</body>
+</html>
+]]
+
+return lustache:render(template, view),
+	{['Content-Type']='text/html; charset=utf-8'}
