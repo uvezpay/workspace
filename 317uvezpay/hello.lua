08ccada7a3f8d2d2bcec24f8dc6d0a16f97d2526
317uvezpay <317uvezpay@gmail.com>
2017-08-09 16:24:37 +0000

    sq1

--- 317uvezpay/hello.lua
+++ 317uvezpay/hello.lua
@@ -1,17 +1,1 @@
-local template =
-[[
-<!doctype html>
-<html>
-<head>
-	<title>blimey</title>
-</head>
-<body>
- bloody cricket
-</body>
-</html>
-]]
- 
-
-local lustache = require 'lustache'
-return lustache:render(template),
-	{['Content-Type']='text/html; charset=utf-8'}
+return "Back to square 1"
